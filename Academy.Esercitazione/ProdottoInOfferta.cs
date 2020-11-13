bda8e12fe05f90381137f8b0802c165ad9aabbff
Serena Cusinato <cusinatoserena@gmail.com>
2020-11-13 15:48:40 +0100

    Add project files.

---
+++ Academy.Esercitazione/ProdottoInOfferta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academy.Esercitazione
+{
+    class ProdottoInOfferta : Prodotto
+    {
+        public DateTime dataInizio { get; set; }
+        public DateTime dataFine { get; set; }
+        public ProdottoInOfferta(string descrip, double price, double sale, int code, DateTime inizio, DateTime fine) : base(descrip, price, sale, code)
+        {
+            this.dataFine = fine;
+            this.dataInizio = inizio;
+        }
+        public ProdottoInOfferta(string descrip, double price, double sale, DateTime inizio, DateTime fine) : base(descrip, price, sale)
+        {
+            this.dataFine = fine;
+            this.dataInizio = inizio;
+        }
+        public ProdottoInOfferta(string descrip, DateTime inizio, DateTime fine) : base(descrip)
+        {
+
+            this.dataFine = fine;
+            this.dataInizio = inizio;
+        }
+        public bool IsInOfferta()
+        {
+            DateTime today = DateTime.Now;
+            return (today >= dataInizio & today <= dataFine.AddDays(1));
+        }
+    }
+}
